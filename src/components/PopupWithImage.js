f4d60363eb4948ca54a12b52cb44680952740e7b
Dinara M <d9268843743@yandex.ru>
2023-06-11 14:56:24 +0200

    Восьмой спринт 8.4

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -6,15 +6,10 @@ export default class PopupWithImage extends Popup{
     this._popupImage = this._popupElement.querySelector('.popup__image');
     this._popupCaption = this._popupElement.querySelector('.popup__title_type_images');
 }
-handleCardClick({name, link}) {
+open({name, link}) {
   this._popupImage.src = link;
   this._popupImage.alt = name;
   this._popupCaption.textContent = name;
   super.open();
 }
 }
-const popupWithImage = new PopupWithImage('.popup_type_images');
-const closeBtnPopupImages = document.querySelector('.popup__close-btn_size_l');
-closeBtnPopupImages.addEventListener('click', () => {
-  popupWithImage.close();
-});
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,10 @@ import { FormValidator } from '../components/FormValidator.js';
 import Popup from '../components/Popup.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import PopupWithForm from '../components/PopupWithForm.js';
-const popupEditProfile = new Popup('.popup_type_edit-profile');
 const openBtnEditProfilePopup = document.querySelector('.profile__edit-button');
 const formElementEditProfile = document.querySelector('.popup__form-element_type_edit-profile');
 const editProfileNameInput = formElementEditProfile.querySelector('.popup__text_type_name');
 const editProfileJobInput = formElementEditProfile.querySelector('.popup__text_type_job');
-const popupNewPlace = new Popup('.popup_type_new-place');
 const openBtnPopupNewPlace = document.querySelector('.profile__add-button');
 const formElementNewPlace = document.querySelector('.popup__form-element_type_new-place');
 const inputNewPlaceTitle = formElementNewPlace.querySelector('.popup__text_type_title');
@@ -21,21 +19,19 @@ const popupWithImage = new PopupWithImage('.popup_type_images');
 const templateSelector = '#group-template';
 const createNewCard = (cardData) => {
   const handleCardClick = (data) => {
-    popupWithImage.handleCardClick(data);
+    popupWithImage.open(data);
   };
   const card = new Card(cardData, templateSelector, handleCardClick);
   return card.generateCard();;
 };
-const popupWithForm = new PopupWithForm('.popup_type_new-place', (inputValues) => {
+const popupNewPlace = new PopupWithForm('.popup_type_new-place', (inputValues) => {
   const cardData = {
     name: inputValues.title,
     link: inputValues.link
   };
     const newCard = createNewCard(cardData);
     section.addItem(newCard);
-    popupWithForm.close();
 });
-popupWithForm.setEventListeners();
 const userInfo = new UserInfo({
   nameSelector: '.profile__title',
   infoSelector: '.profile__subtitle'
@@ -48,17 +44,19 @@ const section = new Section({
   }
 }, '.group');
 section.renderInitialItems();
-function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
-  const name = editProfileNameInput.value;
-  const info = editProfileJobInput.value;
-  userInfo.setUserInfo({ name, info });
-  popupEditProfile.close();
-}
 const resetFormInputs = () => {
   inputNewPlaceTitle.value = '';
   inputNewPlaceLink.value = '';
 }
+popupNewPlace.close = function() {
+  Popup.prototype.close.call(this);
+  resetFormInputs();
+};
+const popupEditProfile = new PopupWithForm('.popup_type_edit-profile', (inputValues) => {
+  const name = inputValues.firstname;
+  const info = inputValues.job;
+  userInfo.setUserInfo({ name, info });
+});
 openBtnEditProfilePopup.addEventListener('click', () => {
   const { name, info } = userInfo.getUserInfo();
   editProfileNameInput.value = name;
@@ -66,13 +64,11 @@ openBtnEditProfilePopup.addEventListener('click', () => {
   popupEditProfile.open();
   formValidatorEditProfile.resetValidation();
 });
-formElementEditProfile.addEventListener('submit', handleProfileFormSubmit);
-popupEditProfile.setEventListeners();
 openBtnPopupNewPlace.addEventListener('click', () => {
   popupNewPlace.open();
-  resetFormInputs();
   formValidatorNewPlace.resetValidation();
 });
+popupEditProfile.setEventListeners();
 popupNewPlace.setEventListeners();
 popupWithImage.setEventListeners();
 const formValidatorEditProfile = new FormValidator(validationConfig, formElementEditProfile);
